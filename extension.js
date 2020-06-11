8d04e118394d4bb443bfae26b98fbe0476b45a5b
tommaso <tommimon@gmail.com>
2020-06-11 23:33:13 +0200

    Improved log messages Added function mylog for custom extension log and used for all the log messages. Translated italian log messages

--- extension.js
+++ extension.js
@@ -4,41 +4,37 @@ const Shell = imports.gi.Shell;
 // Save the standard Menu globally to be able to reset it
 var ParentMenu;
 
+function myLog(text) {
+    log("add-to-desktop: " + text);
+}
+
 function init () {
     ParentMenu = AppDisplay.AppIconMenu;
-    log("ollare: ready");
+    myLog("init completed");
 }
 
 function enable () {
     // AppIconMenu is the var which contains the class used to instantiate the context menu
     // Assigning my own custom menu that extends the standard menu so the custom will be used for instantiate
-    AppDisplay.AppIconMenu = class customMenu extends ParentMenu
-    {
-        _rebuildMenu()
-        {
+    AppDisplay.AppIconMenu = class customMenu extends ParentMenu {
+        _rebuildMenu() {
             super._rebuildMenu();
 
             // Add the "Add to Desktop" entry to the menu
             this._appendSeparator();
             let item = this._appendMenuItem("Add to Desktop");
             item.connect('activate', () => {
-                log("OLLARE: premuto");
+                let appPath = this._source.app.get_app_info().get_filename();
+                myLog(appPath);
             });
-
-            log(this._source.app.get_app_info().get_filename());
-
-            log("OLLARE: override finita");
         }
     }
-    var AppIconMenu = AppDisplay.AppIconMenu;
-    log("ollare: funziona");
-    log(AppIconMenu);
 
-    log("ollare: attivata");
+    myLog("enable completed");
 }
 
 function disable () {
     // Reset the menu to the standard one (not customized)
     AppDisplay.AppIconMenu = ParentMenu;
-    log("ollare: disattivata");
+    myLog("disable completed");
 }
