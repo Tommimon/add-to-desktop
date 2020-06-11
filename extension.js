fade98fa4930dde94991fb4589a94960875db962
tommaso <tommimon@gmail.com>
2020-06-12 00:47:20 +0200

    Now use system desktop Instead of hardcoding the Desktop path now it uses the environment variable. Noe the function that adds the separator and the "Add to Desktop" line is separated in the code and takes the context menu as parameter

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 const AppDisplay = imports.ui.appDisplay;
 const GLib = imports.gi.GLib;
 
-const DESKTOP_DIRECTORY = "/home/user/Desktop";
+const DESKTOP_DIRECTORY = GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_DESKTOP);
 
 // Save the standard Menu globally to be able to reset it
 var ParentMenu;
@@ -15,25 +15,29 @@ function init () {
     myLog("init completed");
 }
 
+function insertCustomEntry(menu) {
+    // Add the "Add to Desktop" entry to the menu
+    menu._appendSeparator();
+    let item = menu._appendMenuItem("Add to Desktop");
+    item.connect('activate', () => {
+        let appPath = menu._source.app.get_app_info().get_filename(); // get the .desktop file complete path
+        myLog(appPath);
+        // create a soft symbolic link on the desktop
+        GLib.spawn_command_line_sync("ln -s " + appPath + " " + DESKTOP_DIRECTORY);
+    });
+}
+
 function enable () {
     // AppIconMenu is the var which contains the class used to instantiate the context menu
     // Assigning my own custom menu that extends the standard menu so the custom will be used for instantiate
     AppDisplay.AppIconMenu = class customMenu extends ParentMenu {
         _rebuildMenu() {
             super._rebuildMenu();
-
-            // Add the "Add to Desktop" entry to the menu
-            this._appendSeparator();
-            let item = this._appendMenuItem("Add to Desktop");
-            item.connect('activate', () => {
-                let appPath = this._source.app.get_app_info().get_filename(); // get the .desktop file complete path
-                myLog(appPath);
-                // create a soft symbolic link on the desktop
-                GLib.spawn_command_line_sync("ln -s " + appPath + " " + DESKTOP_DIRECTORY);
-            });
+            insertCustomEntry(this);
         }
     }
 
+    myLog("DESKTOP_DIRECTORY = "  + DESKTOP_DIRECTORY);
     myLog("enable completed");
 }
 
