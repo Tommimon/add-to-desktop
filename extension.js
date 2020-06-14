860b49e3ae44ba20a58fa9956fc1c5dc0b756878
Tommimon <37435103+Tommimon@users.noreply.github.com>
2020-06-14 13:14:48 +0200

    Added command to change permissions This code is executed always, is missing the control to check if it is necessary

--- extension.js
+++ extension.js
@@ -1,5 +1,6 @@
 const AppDisplay = imports.ui.appDisplay;
 const GLib = imports.gi.GLib;
+const Gio = imports.gi.Gio;
 
 const DESKTOP_DIRECTORY = GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_DESKTOP);
 
@@ -22,8 +23,8 @@ function insertCustomEntry(menu) {
     item.connect('activate', () => {
         let appPath = menu._source.app.get_app_info().get_filename(); // get the .desktop file complete path
         myLog(appPath);
-        // create a soft symbolic link on the desktop
-        GLib.spawn_command_line_sync("ln -s " + appPath + " " + DESKTOP_DIRECTORY);
+        let handler = new PermissionsHandler(appPath);
+        handler.handlePermissions();
     });
 }
 
@@ -45,4 +46,77 @@ function disable () {
     // Reset the menu to the standard one (not customized)
     AppDisplay.AppIconMenu = ParentMenu;
     myLog("disable completed");
+}
+
+class PermissionsHandler {
+    constructor(appPath) {
+        this.appPath = appPath;
+    }
+
+    // requires authentication (if needed) and creates link
+    handlePermissions() {
+        if(true) {
+            this.fixPermissions();
+        }
+        else {
+            this.createLink();
+        }
+    }
+
+    fixPermissions() {
+        let args = ["chmod", "755", this.appPath];
+        privelegedExec(args, (out, err) => {
+            myLog("arrivato");
+            this.chmodCompleted(out, err);
+        });
+    }
+
+    chmodCompleted(out, err) {
+        myLog("arrivatissimo");
+        // if we have successfully changed the permissions we create the link
+        if(err === undefined) {
+            this.createLink(this.appPath);
+        }
+        else {
+            myLog("Authentication failed");
+        }
+    }
+
+    createLink() {
+        // create a soft symbolic link on the desktop
+        GLib.spawn_command_line_async("ln -s " + this.appPath + " " + DESKTOP_DIRECTORY);
+    }
+
+}
+
+// executes command as root asking for authentication and pass the result to the function onFinish
+function privelegedExec(args, onFinish) {
+    try {
+        let proc = Gio.Subprocess.new(
+            ['pkexec'].concat(args),
+            Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE
+        );
+
+        proc.communicate_utf8_async(null, null, (proc, res) => {
+            let out, err;
+
+            try {
+                let [, stdout, stderr] = proc.communicate_utf8_finish(res);
+
+                // Failure
+                if (!proc.get_successful())
+                    throw new Error(stderr);
+
+                // Success
+                out = stdout;
+            } catch (e) {
+                err = e;
+            }
+
+            onFinish(out, err); // the async errors and outputs are passed to another function
+        });
+    } catch (e) {
+        return e; // the immediate errors are returned to caller
+    }
+    return null;
 }
