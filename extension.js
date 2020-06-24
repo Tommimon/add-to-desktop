a55e573fe9fdaaf13b3c9791868df2be6a338aac
Tommimon <37435103+Tommimon@users.noreply.github.com>
2020-06-24 22:19:22 +0200

    Rewrite code to use Gio info instead of ll command, changed permissions required to create a new shortcut
    The vast majority of the extension work is in shortcutMaker.js: in LauncherInfo are contained the info retrieved with Gio about the application launcher file, ShortcutMaker decides what to do using LauncherInfo, changes permissions and creates the link
    
    The required permissions now are:
    - No write permission for others
    - Execute permission for everyone if the owner isn't the current user

--- extension.js
+++ extension.js
@@ -1,50 +1,22 @@
 const AppDisplay = imports.ui.appDisplay;
-const GLib = imports.gi.GLib;
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
-const PermissionsHandler = Me.imports.permissionsHandler;
-const MyLog = Me.imports.myLog.MyLog;
+const ShortcutMaker = Me.imports.shortcutMaker;
 
-// var because accessed elsewhere
-var DESKTOP_DIRECTORY = GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_DESKTOP);
-
-// Save the standard Menu globally to be able to reset it
-var ParentMenu;
+// Saves the standard Menu globally to be able to reset it on disable
+var parentMenu = null;
 
 function init () {
-    ParentMenu = AppDisplay.AppIconMenu;
-    MyLog("init completed");
-}
 
-function insertCustomEntry(menu) {
-    // Add the "Add to Desktop" entry to the menu
-    menu._appendSeparator();
-    let item = menu._appendMenuItem("Add to Desktop");
-    item.connect('activate', () => {
-        let appPath = menu._source.app.get_app_info().get_filename(); // get the .desktop file complete path
-        MyLog(appPath);
-        let handler = new PermissionsHandler.PermissionsHandler(appPath);
-        handler.start();
-    });
 }
 
 function enable () {
-    // AppIconMenu is the var which contains the class used to instantiate the context menu
-    // Assigning my own custom menu that extends the standard menu so the custom will be used for instantiate
-    AppDisplay.AppIconMenu = class customMenu extends ParentMenu {
-        _rebuildMenu() {
-            super._rebuildMenu();
-            insertCustomEntry(this);
-        }
-    }
-
-    MyLog("DESKTOP_DIRECTORY = "  + DESKTOP_DIRECTORY);
-    MyLog("enable completed");
+    parentMenu = AppDisplay.AppIconMenu;
+    ShortcutMaker.addShortcutButton(parentMenu);
 }
 
 function disable () {
-    // Reset the menu to the standard one (not customized)
-    AppDisplay.AppIconMenu = ParentMenu;
-    MyLog("disable completed");
+    // Reset the menu to the standard one (without new item)
+    AppDisplay.AppIconMenu = parentMenu;
 }
