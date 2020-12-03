8d109fe27e062b58da9fdd755a76aa1e2153af8d
Tommimon <37435103+Tommimon@users.noreply.github.com>
2020-12-03 19:40:27 +0100

    Using copy instead of link

--- shortcutMaker.js
+++ shortcutMaker.js
@@ -3,7 +3,7 @@ const GLib = imports.gi.GLib;
 const AppDisplay = imports.ui.appDisplay;
 const PopupMenu = imports.ui.popupMenu;
 
-var S_IWOTH = 0o00002;
+var DEFAULT_AND_UEXEC = 0o00764;
 
 // executes command and pass the result to the callback function
 function execAsync(args, callback) {
@@ -73,126 +73,66 @@ function insertAddToDesktopButton(menu) {
 
     item.connect('activate', () => {
         let appPath = menu._source.app.get_app_info().get_filename(); // get the .desktop file complete path
-        let shortcutMaker = new ShortcutMaker(appPath);
-        shortcutMaker.makeShortcut();
+        let shortcutMaker = new ShortcutMaker();
+        shortcutMaker.makeShortcut(appPath);
     });
 }
 
-// decides which permission are required, adds them, creates the shortcut and allows launching
-class ShortcutMaker {
-    constructor(appPath) {
-        this._appPath = appPath;
+
+var ShortcutMaker = class ShortcutMaker {
+    constructor() {
         this._desktop = GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_DESKTOP);
-        this._info = new LauncherInfo(appPath);
-        this._current = GLib.get_user_name();
+        this._appPath = null;
+        this._appFile = null;
+        this._copyPath = null;
+        this._shortcutFile = null;
     }
 
-    makeShortcut() {
-        this._info.initInfo(() => {this._decideAction()});
+    // calculates the copy path from the desktop path
+    getCopyPath() {
+        let array = this._appPath.split("/");
+        let name = array.pop();
+        return this._desktop + '/' + name;
     }
 
-
-    _decideAction() {
-        log('Current user = ' + this._current);
-        log('Owner = ' + this._info.owner);
-        log('Can execute = ' + this._info.canExecute);
-        log('Writable by others = ' + this._info.writableByOthers);
-
-        if(this._current === this._info.owner) {
-            if(this._info.writableByOthers) {
-                log('Removing write bit for others');
-                this._fixPermissions();
-            }
-            else {
-                log('No extra permissions needed');
-                this._createLink();
-            }
-        }
-        else {
-            let fixWrite = this._info.writableByOthers;
-            let fixExec = !this._info.canExecute;
-            if(fixWrite || fixExec) {
-                log('Requiring authentication to fix permissions');
-                this._sudoFixPermissions(fixWrite, fixExec);
-            }
-            else {
-                log('No extra permissions needed');
-                this._createLink();
-            }
-        }
+    makeShortcut(appPath) {
+        this._appPath = appPath;
+        this._appFile = Gio.File.new_for_path(this._appPath);
+        this._copyPath = this.getCopyPath();
+        this._shortcutFile = Gio.File.new_for_path(this._copyPath);
+        this._createCopy();
     }
 
-    _fixPermissions() {
-        // the only action needed is remove writable by others
-        let args = ['chmod', 'o-w', this._appPath];
-        execAsync(args, (out, err) => {
-            if(err === undefined) {
-                this._createLink();
-            }
-            else {
-                log(err);  // unexpected error
-            }
-        });
-    }
-
-    calcMode(fixWrite, fixExec) {
-        let mode = '';
-        if(fixWrite) {
-            mode = 'o-w';
-            if(fixExec) {
-                mode = 'o-w,a+x';
-            }
-        }
-        else {
-            if(fixExec) {
-                mode = 'a+x';
-            }
-        }
-        return mode;
-    }
-
-    // require permission before executing command as root, using Gio subprocess to use pkexec
-    _sudoFixPermissions(fixWrite, fixExec) {
-        let mode = this.calcMode(fixWrite, fixExec);
-        let args = ['pkexec', 'chmod', mode, this._appPath];
-        execAsync(args, (out, err) => {
-            // if we have successfully changed the permissions we create the link
-            if(err === undefined) {
-                log('Authenticated successfully');
-                this._createLink(this._appPath);
-            }
-            else {
-                log(err);  // may be request dismissed
-            }
-        });
-    }
-
-    _createLink() {
-        // create a soft symbolic link on the desktop
-        execAsync(['ln', '-s', this._appPath, this._desktop], () => {});
-    }
-}
-
-// contains the info about the .desktop file
-class LauncherInfo {
-    constructor(appPath) {
-        this.file = Gio.File.new_for_path(appPath);
-    }
-
-    initInfo(callback) {
-        this.file.query_info_async(
-            'access::can-execute,owner::user,unix::mode',
-            Gio.FileQueryInfoFlags.NONE,
+    _createCopy() {
+        this._appFile.copy_async(
+            this._shortcutFile,
+            Gio.FileCopyFlags.OVERWRITE | Gio.FileCopyFlags.TARGET_DEFAULT_PERMS,
             GLib.PRIORITY_DEFAULT,
             null,
+            null,
             (source, result) => {
-                let fileInfo = source.query_info_finish(result);
-                this.owner = fileInfo.get_attribute_as_string('owner::user');
-                this.canExecute = fileInfo.get_attribute_boolean('access::can-execute');
-                let unixMode = fileInfo.get_attribute_uint32('unix::mode');
-                this.writableByOthers = (unixMode & S_IWOTH) != 0;
-                callback();
-            }
-        )
+                try {
+                    source.copy_finish(result);
+                    this._setExecutableBit();
+                } catch(e) {
+                    log(`Failed to create shortcut ${e.message}`);
+                }
+            });
+    }
+
+    _setExecutableBit() {
+        let new_info = new Gio.FileInfo();
+        new_info.set_attribute_uint32(Gio.FILE_ATTRIBUTE_UNIX_MODE, DEFAULT_AND_UEXEC);
+        this._shortcutFile.set_attributes_async(new_info,
+            Gio.FileQueryInfoFlags.NONE,
+            GLib.PRIORITY_LOW,
+            null,
+            (source, result) => {
+                try {
+                    source.set_attributes_finish (result);
+                } catch(e) {
+                    log(`Failed to set unix mode: ${e.message}`);
+                }
+            });
     }
 }
