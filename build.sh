8e5041621cb649a167e89452ec94a07be5856135
Tommimon <37435103+Tommimon@users.noreply.github.com>
2020-12-07 23:39:46 +0100

    Build script creates release .zip

--- build.sh
+++ build.sh
@@ -1,8 +1,21 @@
 # compile language
 mkdir locale
 for f in po/*; do
-  name=${f:3:${#str}-3};
-  mkdir locale/"$name";
-  mkdir locale/"$name"/LC_MESSAGES;
-  msgfmt "$f" --output-file=locale/"$name"/LC_MESSAGES/add-to-desktop.mo;
+    name=${f:3:${#f}-6};
+    mkdir locale/"$name";
+    mkdir locale/"$name"/LC_MESSAGES;
+    msgfmt "$f" --output-file=locale/"$name"/LC_MESSAGES/add-to-desktop.mo;
 done
+
+# find version
+while read line
+do
+    if echo "$line" | grep -q '"version" :'
+    then
+        version=${line:12:${#line}-12};
+    fi
+done < metadata.json
+
+# create .zip
+mkdir output
+zip output/add-to-desktop@tommimon.github.com.v"${version}".shell-extension.zip ./*.js README.md LICENSE metadata.json -r locale
